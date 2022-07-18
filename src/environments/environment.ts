1ed7c6d18511ae281c8d87458fb50347bd6f609b
cdickson <info@calebdickson.com>
2022-07-18 01:27:18 -0400

    Updated version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
-  version: '0.5.2',
-  lastUpdate: new Date(2022, 0o6, 0o17, 17, 0, 0, 0)
+  version: '0.5.3',
+  lastUpdate: new Date(2022, 0o6, 18, 17, 0, 0, 0)
 };
